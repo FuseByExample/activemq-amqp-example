5e7a4d423bc35fb6d018327e08a20a71b5f0a098
Kevin Earls <kevin@kevinearls.com>
2013-11-20 18:42:27 +0100

    Changes to enable reproducing ENTMQ466

--- src/main/java/org/fusebyexample/activemq/SimpleProducer.java
+++ src/main/java/org/fusebyexample/activemq/SimpleProducer.java
@@ -21,6 +21,7 @@ import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import org.apache.qpid.amqp_1_0.jms.impl.ConnectionFactoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,32 +29,25 @@ public class SimpleProducer {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleProducer.class);
 
     private static final Boolean NON_TRANSACTED = false;
-    private static final long MESSAGE_TIME_TO_LIVE_MILLISECONDS = 0;
     private static final int MESSAGE_DELAY_MILLISECONDS = 100;
     private static final int NUM_MESSAGES_TO_BE_SENT = 100;
-    private static final String CONNECTION_FACTORY_NAME = "myJmsFactory";
-    private static final String DESTINATION_NAME = "queue/simple";
+    public static final String DESTINATION_NAME = "topic://test.topic.simple";
 
     public static void main(String args[]) {
         Connection connection = null;
 
         try {
-            // JNDI lookup of JMS Connection Factory and JMS Destination
-            Context context = new InitialContext();
-            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
-            Destination destination = (Destination) context.lookup(DESTINATION_NAME);
-
-            connection = factory.createConnection();
+            ConnectionFactory connectionFactory = (ConnectionFactory) new ConnectionFactoryImpl("localhost", 5672, "admin", "admin");
+            connection = connectionFactory.createConnection(/*"admin", "admin"*/);
             connection.start();
 
             Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createTopic(DESTINATION_NAME);
             MessageProducer producer = session.createProducer(destination);
 
-            producer.setTimeToLive(MESSAGE_TIME_TO_LIVE_MILLISECONDS);
-
             for (int i = 1; i <= NUM_MESSAGES_TO_BE_SENT; i++) {
-                TextMessage message = session.createTextMessage(i + ". message sent");
-                LOG.info("Sending to destination: " + destination.toString() + " this text: '" + message.getText());
+                TextMessage message = session.createTextMessage("Message " + i);
+                LOG.info("Sending to destination: [" + DESTINATION_NAME + "] this text: '" + message.getText());
                 producer.send(message);
                 Thread.sleep(MESSAGE_DELAY_MILLISECONDS);
             }
@@ -64,11 +58,6 @@ public class SimpleProducer {
         } catch (Throwable t) {
             LOG.error("Error sending message", t);
         } finally {
-            // Cleanup code
-            // In general, you should always close producers, consumers,
-            // sessions, and connections in reverse order of creation.
-            // For this simple example, a JMS connection.close will
-            // clean up all other resources.
             if (connection != null) {
                 try {
                     connection.close();
